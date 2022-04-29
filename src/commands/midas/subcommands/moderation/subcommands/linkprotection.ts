6e757f3db1c2dbef3dfad58499dd77227199e7d4
Ashen <ashen@ashen.live>
2022-04-29 17:13:21 +0100

    Added support for banned links even for excluded members.

--- src/commands/midas/subcommands/moderation/subcommands/linkprotection.ts
+++ src/commands/midas/subcommands/moderation/subcommands/linkprotection.ts
@@ -1,6 +1,6 @@
 import { Command } from "@guildedjs/gil";
-import { Message } from "guilded.js";
 import consola from "consola"
+import { Message } from "guilded.js/types/structures";
 const config = require('../../../../../config')
 consola.log('✅ Loaded moderation-linkprotection command.')
 
@@ -21,7 +21,7 @@ export default class BotCommand extends Command {
         name: "text",
         type: "...string",
       }] as const
-    parentCommand = "moderation"
+    parentCommand = "midas-moderation"
     async execute(message: Message, args: { subcommand: string, text: any}) {
         switch (args.subcommand) {
             case "add":
@@ -31,7 +31,8 @@ export default class BotCommand extends Command {
                     const urlPattern = new RegExp(/\[([^\[]+)\](\(.*\))/gm)
                     if (args.text.match(urlPattern)) {
                         let matched = args.text.replace(/\[([^\]]+)\][^\)]+\)/g, '$1') // This strips the markdown formatting off of the link and outputs just the raw text.
-                        return await message.send(config.configuration.lang.links.approved_link)
+                        let domain = matched.match(/([^\/,\s]+\.[^\/,\s]+?)(?=\/|,|\s|$|\?|#)/g)
+                        return await message.send(config.configuration.lang.links.blocked_link)
                     } else {
                         return await message.send(config.configuration.lang.links.invalid_link)
                     }
@@ -43,9 +44,15 @@ export default class BotCommand extends Command {
                     const urlPattern = new RegExp(/\[([^\[]+)\](\(.*\))/gm)
                     if (args.text.match(urlPattern)) {
                         let matched = args.text.replace(/\[([^\]]+)\][^\)]+\)/g, '$1') // This strips the markdown formatting off of the link and outputs just the raw text.
-                        return await message.send(config.configuration.lang.links.blocked_link)
+                        let domain = matched.match(/([^\/,\s]+\.[^\/,\s]+?)(?=\/|,|\s|$|\?|#)/g)
+                        const bannedLinks = config.configuration.links.banned_links;
+                        const isBannedLink = bannedLinks.includes(domain);
+                        if(isBannedLink) {
+                            //Remove from the banned links list...
+                        }
+                        return await message.send(config.configuration.lang.links.approved_link)
                     } else {
-                        return await message.send('')
+                        return
                     }
                 }
             default:
--- src/monitors/links.ts
+++ src/monitors/links.ts
@@ -1,33 +1,52 @@
-import { RESTPostChannelMessagesBody } from './../../node_modules/@guildedjs/guilded-api-typings/dist/v1/rest/Message.d';
-import type { Message } from 'guilded.js'
-import { Monitor } from '@guildedjs/gil'
-import consola from 'consola'
-const config = require('../config')
-consola.info('Loaded links Monitor...')
+import { RESTPostChannelMessagesBody } from "./../../node_modules/@guildedjs/guilded-api-typings/dist/v1/rest/Message.d";
+import type { Message } from "guilded.js";
+import { Monitor } from "@guildedjs/gil";
+import consola from "consola";
+const config = require("../config");
+consola.info("Loaded links Monitor...");
 export class LinksMonitor extends Monitor {
-
-    async execute(message: Message): Promise<unknown> {
-
-        let urlPattern = new RegExp(/\[([^\[]+)\](\(.*\))/gm)
-        if (!message.content.match(urlPattern)) return;
-
-        const roles = await this.client.members.getRoles(config.configuration.server_id, message.authorId);
-        const exemptRoles = config.configuration.links.exempt_roles
-        const hasExemption = exemptRoles.every((role: number) => roles.includes(role));
-        if(hasExemption) return;
+  async execute(message: Message): Promise<unknown> {
+    let urlPattern = new RegExp(/\[([^\[]+)\](\(.*\))/gm);
+    if (!message.content.match(urlPattern)) return;
+    let matched = message.content.replace(/\[([^\]]+)\][^\)]+\)/g, "$1"); // This strips the markdown formatting off of the link and outputs just the raw text.
+    let domain = matched.match(/([^\/,\s]+\.[^\/,\s]+?)(?=\/|,|\s|$|\?|#)/g)
+    const roles = await this.client.members.getRoles(
+      config.configuration.server_id,
+      message.authorId
+    );
+    const exemptRoles = config.configuration.links.exempt_roles;
+    const hasExemption = exemptRoles.every((role: number) =>
+      roles.includes(role)
+    );
+    if (hasExemption) {
+      const bannedLinks: Array<String> = config.configuration.links.banned_links;
+      const isBannedLink = bannedLinks.every((link: any) => domain?.includes(link))
+      consola.info(domain, bannedLinks)
+      if (isBannedLink) {
+        consola.warn('Link posted with banned domain:', domain)
         let response: RESTPostChannelMessagesBody = {
-            isPrivate: true,
-            replyMessageIds: [message.id],
-            content: config.configuration.lang.links.deleted_link,
-
-        }
-        message.send(response)
-        this.client.messages.delete(message.channelId, message.id)
+          isPrivate: true,
+          replyMessageIds: [message.id],
+          content: config.configuration.lang.links.banned_link,
+        };
+        message.send(response);
+        this.client.messages.delete(message.channelId, message.id);
+        return
+      }
+      return
     }
+    let response: RESTPostChannelMessagesBody = {
+      isPrivate: true,
+      replyMessageIds: [message.id],
+      content: config.configuration.lang.links.deleted_link,
+    };
+    message.send(response);
+    this.client.messages.delete(message.channelId, message.id);
+  }
 
-    init(): unknown {
-        return void 0;
-    }
+  init(): unknown {
+    return void 0;
+  }
 }
 
 export default LinksMonitor;
